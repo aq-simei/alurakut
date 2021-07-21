aacc5c7341bf4717835d2791153335f5b07e7721
Antonio Simei <aq.simei@gmail.com>
2021-07-21 19:35:15 -0300

    Início do Alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,79 @@
-import styled from 'styled-components'
+import { Box } from "../src/components/Box";
+import { MainGrid } from "../src/components/MainGrid";
+import { AlurakutMenu, OrkutNostalgicIconSet } from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileInteractions";
+//const Title = styled.h1`
+//  font-size: 50px;
+//  color: ${({ theme }) => theme.colors.primary};`
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const usuarioInd = "aq-simei";
+
+const pessoasFavoritas = [
+  "MatheusSanchez",
+  "omariosouto",
+  "juunegreiros",
+  "peas",
+  "maykbrito",
+  "BrunaRP",
+  "diego3g",
+];
+
+function ProfileSidebar(props) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${props.gitHubUser}.png`}
+        alt="Foto de perfil"
+      />
+    </Box>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar gitHubUser={usuarioInd} />
+        </div>
+
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box className="title">
+            <h1>
+              Bem-vindo(a)
+            </h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade {pessoasFavoritas.length}
+            </h2>
+
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img
+                        src={`https://github.com/${itemAtual}.png`}
+                        alt="Foto de usuário"
+                      />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                );  
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
---
+++ src/components/MainGrid/index.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const MainGrid = styled.main`
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
+  grid-gap: 10px;
+  padding: 16px;
+
+  .profileArea{
+    display: none;
+    @media(min-width:860px){
+      display: block;
+    }
+    img{
+        border-radius: 8px;
+    }
+  }
+  @media(min-width:860px){
+    max-width: 1110px;
+    display: grid;
+    grid-template-columns: 160px 1fr 312px ;
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+  }
+`;
